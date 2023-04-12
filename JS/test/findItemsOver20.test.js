5e83af559046928483eafe4e28a3c0ef59b798b0
LeCodeGuy <devanj89@gmail.com>
2023-04-12 11:25:51 +0200

    moved the variables into the it blocks for the findItemsOver,findItemsOver20 and mostProfitableDepartment tests

--- JS/test/findItemsOver20.test.js
+++ JS/test/findItemsOver20.test.js
@@ -1,32 +1,58 @@
-var results = [
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-];
-var results2 = [];
-
-var results3 = [
-    {name : 'apples', qty : 40},
-    {name : 'bananas', qty : 23},
-    {name : 'apples', qty : 37}
-];
-
 describe('Test my findItemsOver20 function' , function(){
     it('Should return results when called with "itemList"' , function(){
+        var itemList = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+            {name : 'apples', qty : 3},
+        ];
+        
+        var results = [
+            {name : 'pears', qty : 37},
+            {name : 'bananas', qty : 27},
+        ];
+
         assert.deepEqual(results, findItemsOver20(itemList));
     });
 
     it('Should return results2 when called with "itemList2"' , function(){
+        var itemList2 = [
+            {name : 'apples', qty : 10},
+            {name : 'pears', qty : 19},
+            {name : 'bananas', qty : 17},
+            {name : 'apples', qty : 3},
+        ];
+        
+        var results2 = [];
+        
         assert.deepEqual(results2, findItemsOver20(itemList2));
     });
 
     it('Should return results3 when called with "itemList3"' , function(){
+        var itemList3 = [
+            {name : 'apples', qty : 40},
+            {name : 'pears', qty : 20},
+            {name : 'bananas', qty : 23},
+            {name : 'apples', qty : 37}
+        ];
+
+        var results3 = [
+            {name : 'apples', qty : 40},
+            {name : 'bananas', qty : 23},
+            {name : 'apples', qty : 37}
+        ];
+        
         assert.deepEqual(results3, findItemsOver20(itemList3));
     });
 
     it('Should return results2 when called with no parameter passed' , function(){
+        var results2 = [];
+
         assert.deepEqual(results2, findItemsOver20());
     });
     it('Should return results2 when called with "Hello"' , function(){
+        var results2 = [];
+        
         assert.deepEqual(results2, findItemsOver20("Hello"));
     });
 })
